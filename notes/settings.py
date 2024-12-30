844e116082fe61c136187b79d150924326e5b5a2
ExcelsKennedy2 <kennedymbugua841@gmail.com>
2024-12-30 19:01:30 +0300

    third commit

--- notes/settings.py
+++ notes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*d=amg8vqjx52m@8)kkd(t7ku7ccqcl7rtio41t)m!19kt^ugm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simple-notes-app-g4ec.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
